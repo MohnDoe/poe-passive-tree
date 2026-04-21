41428e70ea046fbdac0102a573d4429bb37d2a40
mohndoe <mohndoe@pm.me>
2026-04-21 14:35:21 +0200

    wip: curved links

---
+++ src/pixi/tree/links.renderer.ts
@@ -0,0 +1,65 @@
+import type { NodeId, PassiveNode } from "@/domain/models/passiveNode";
+import type { PassiveTreeNodesById, PassiveTreeAdjacency, PassiveTree } from "@/domain/models/passiveTree";
+import { Graphics } from "pixi.js";
+import type { PixiStageController } from "../stage";
+
+type Edge = readonly [NodeId, NodeId];
+export function renderLinks(stage: PixiStageController, tree: PassiveTree, adj: PassiveTreeAdjacency) {
+  const links = new Graphics();
+  const nodes = tree.nodesById;
+
+  for (const [nodeAId, nodeBId] of uniqueEdgesFromAdjacency(adj)) {
+    const nodeA = nodes.get(nodeAId);
+    const nodeB = nodes.get(nodeBId);
+
+    if (!nodeA || !nodeB) continue;
+    if (!nodeA.position || !nodeB.position) continue;
+
+    if (shouldRenderAsArc(nodeA, nodeB)) {
+      const startPoint = nodeA.position;
+      const endPoint = nodeB.position;
+      const groupId = nodeA.groupId!;
+
+      const groupCenter = tree.groups.get(groupId)!;
+      const radius = Math.hypot(startPoint.x - groupCenter.x, startPoint.y - groupCenter.y);
+      const startAngle = Math.atan2(startPoint.y - groupCenter.y, startPoint.x - groupCenter.x);
+      const endAngle = Math.atan2(endPoint.y - groupCenter.y, endPoint.x - groupCenter.x);
+
+      // links.arcTo(startPoint.x, startPoint.y, endPoint.x, endPoint.y, radius)
+
+
+    } else {
+      links
+        .moveTo(nodeA.position.x, nodeA.position.y)
+        .lineTo(nodeB.position.x, nodeB.position.y)
+    }
+
+  }
+
+  links.stroke({
+    color: 0x5f6775,
+    width: 2,
+    alpha: 0.7
+  })
+
+  stage.linkLayer.addChild(links);
+}
+
+
+function uniqueEdgesFromAdjacency(adj: PassiveTreeAdjacency): Edge[] {
+  const edges: Edge[] = [];
+
+  for (const [nodeAId, neighbors] of adj) {
+    for (const nodeBId of neighbors) {
+      if (nodeAId < nodeBId) {
+        edges.push([nodeAId, nodeBId])
+      }
+    }
+  }
+
+  return edges;
+}
+
+function shouldRenderAsArc(a: PassiveNode, b: PassiveNode) {
+  return a.groupId === b.groupId && a.orbit === b.orbit
+}
--- src/pixi/treeRenderer.ts
+++ src/pixi/treeRenderer.ts
@@ -1,7 +1,8 @@
-import type { PassiveTree, PassiveTreeAdjacency, PassiveTreeNodesById } from "@/domain/models/passiveTree";
-import { Circle, Graphics } from "pixi.js";
+import type { PassiveNode, PassiveNodeRegion } from "@/domain/models/passiveNode";
+import type { PassiveTree, PassiveTreeNodesById } from "@/domain/models/passiveTree";
+import { Graphics } from "pixi.js";
 import type { PixiStageController } from "./stage";
-import type { NodeId, PassiveNode, PassiveNodeRegion } from "@/domain/models/passiveNode";
+import { renderLinks } from "./tree/links.renderer";
 
 
 type NodeVisualStyle = {
@@ -16,7 +17,7 @@ type NodeVisualStyle = {
 export function renderTree(stage: PixiStageController, tree: PassiveTree) {
   clearStage(stage);
 
-  renderLinks(stage, tree.nodesById, tree.adjacency.main);
+  renderLinks(stage, tree, tree.adjacency.main);
   renderNodes(stage, tree.nodesById, { region: 'main' });
 }
 
@@ -25,45 +26,6 @@ function clearStage(stage: PixiStageController) {
   stage.linkLayer.removeChildren();
 }
 
-function renderLinks(stage: PixiStageController, nodes: PassiveTreeNodesById, adj: PassiveTreeAdjacency) {
-  const links = new Graphics();
-
-  for (const [nodeAId, nodeBId] of uniqueEdgesFromAdjacency(adj)) {
-    const nodeA = nodes.get(nodeAId);
-    const nodeB = nodes.get(nodeBId);
-
-    if (!nodeA || !nodeB) continue;
-    if (!nodeA.position || !nodeB.position) continue;
-
-    links
-      .moveTo(nodeA.position.x, nodeA.position.y)
-      .lineTo(nodeB.position.x, nodeB.position.y)
-  }
-
-  links.stroke({
-    color: 0x5f6775,
-    width: 2,
-    alpha: 0.7
-  })
-
-  stage.linkLayer.addChild(links);
-}
-
-type Edge = readonly [NodeId, NodeId];
-
-function uniqueEdgesFromAdjacency(adj: PassiveTreeAdjacency): Edge[] {
-  const edges: Edge[] = [];
-
-  for (const [nodeAId, neighbors] of adj) {
-    for (const nodeBId of neighbors) {
-      if (nodeAId < nodeBId) {
-        edges.push([nodeAId, nodeBId])
-      }
-    }
-  }
-
-  return edges;
-}
 
 function renderNodes(stage: PixiStageController, nodes: PassiveTreeNodesById, filter: { region: PassiveNodeRegion }) {
   for (const [nodeId, node] of nodes) {
@@ -114,3 +76,5 @@ function getNodeVisualeStyle(node: PassiveNode): NodeVisualStyle {
 
   return style
 }
+
+
