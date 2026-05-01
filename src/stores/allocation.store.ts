bee836c480730d9eba2fb14987126df751b66c11
mohndoe <mohndoe@pm.me>
2026-05-01 18:15:52 +0200

    feat: allocation store

---
+++ src/stores/allocation.store.ts
@@ -0,0 +1,20 @@
+import { computeAllocationState } from "@/services/passiveTree/allocation/computeAllocationState";
+import { defineStore } from "pinia";
+import { computed } from "vue";
+import { useBuildStore } from "./build.store";
+import { useRuntimeStore } from "./runtime.store";
+
+export const useAllocationStore = defineStore("allocation", () => {
+  const buildStore = useBuildStore();
+  const runtimeStore = useRuntimeStore();
+
+  const allocationState = computed(() => {
+    const { graph } = runtimeStore;
+    const { build } = buildStore;
+
+    if (!graph || build.activeClassId === null) return null;
+    return computeAllocationState({ graph, buildState: build });
+  });
+
+  return { allocationState };
+});
