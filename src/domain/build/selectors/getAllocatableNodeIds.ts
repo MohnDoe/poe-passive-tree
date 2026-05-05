67186b6d58b674aeb8aa89f39081a6aa6986ca55
mohndoe <mohndoe@pm.me>
2026-05-05 15:51:29 +0200

    chore: delete unused selectors getAllocatableNodeIds + getNodeAllocationState

--- src/domain/build/selectors/getAllocatableNodeIds.ts
+++
@@ -1,7 +0,0 @@
-import type { AllocationState } from "../models/allocation/Allocation";
-
-export function getAllocatableNodeIds(
-  snapshot: AllocationState,
-): AllocationState["allocatableNodeIds"] {
-  return snapshot.allocatableNodeIds;
-}
