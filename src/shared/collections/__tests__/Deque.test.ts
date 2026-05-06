1e31df50254f5a6ab7758a34a4378e6a0fef7404
mohndoe <mohndoe@pm.me>
2026-05-06 10:15:47 +0200

    test(deque): spread empty tests and add clear on empty test

--- src/shared/collections/__tests__/Deque.test.ts
+++ src/shared/collections/__tests__/Deque.test.ts
@@ -7,8 +7,15 @@ describe("Deque", () => {
 
     expect(deque.size).toBe(0);
     expect(deque.isEmpty()).toBe(true);
+  });
 
+  it("cannot pop front when empty", () => {
+    const deque = new Deque<number>();
     expect(deque.popFront()).toBeUndefined();
+  });
+
+  it("cannot pop back when empty", () => {
+    const deque = new Deque<number>();
     expect(deque.popBack()).toBeUndefined();
   });
 
@@ -56,18 +63,11 @@ describe("Deque", () => {
     expect(deque.isEmpty()).toBe(true);
   });
 
-  it("tracks size correctly and can be cleared", () => {
+  it("does nothing when clearing empty deque", () => {
     const deque = new Deque<number>();
-    deque.pushBack(1);
-    deque.pushFront(0);
-    deque.pushBack(2);
-
-    expect(deque.size).toBe(3);
-
     deque.clear();
 
     expect(deque.size).toBe(0);
     expect(deque.isEmpty()).toBe(true);
-    expect(deque.popFront()).toBeUndefined();
   });
 });
