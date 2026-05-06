75d380877bcb8caaf5d688b089bbe9476cb1bda1
mohndoe <mohndoe@pm.me>
2026-05-06 09:28:41 +0200

    feat: some simple visual changes

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
 import { onMounted } from "vue";
 import ClassSelector from "./presentation/components/ClassSelector.vue";
 import PassiveTreeCanvas from "./presentation/components/PassiveTreeCanvas.vue";
-import { usePassiveTreeRuntime } from "./presentation/composables/usePassiveTreeRuntime";
 import StatsPanel from "./presentation/components/StatsPanel.vue";
+import { usePassiveTreeRuntime } from "./presentation/composables/usePassiveTreeRuntime";
 
 const { status, error, ensureLoaded } = usePassiveTreeRuntime();
 
@@ -14,15 +14,31 @@ onMounted(() => {
 
 <template>
   <div class="app-layout">
-    <header>
-      <h1>PoE Passive Tree (WIP)</h1>
-      <ClassSelector />
-      <StatsPanel />
-    </header>
     <main>
       <p v-if="status === 'loading'">Loading ...</p>
       <p v-else-if="status === 'error'">{{ error }}</p>
-      <PassiveTreeCanvas v-else-if="status === 'ready'" />
+      <div v-else-if="status === 'ready'">
+        <div class="header">
+          <div class="allocation">
+            <ClassSelector />
+            <StatsPanel />
+          </div>
+        </div>
+        <p class="info">
+          <b class="warning">A work in progress.</b>
+          <br />
+          Recreating Path of Exile's passive tree using <em>TypeScript</em> and <em>Pixi.js</em>.
+          <br />
+          <span class="progress">
+            Core allocation & refund logic ✅<br />
+            Unit tests ⏳️<br />
+            Visual rendering ⏳️
+          </span>
+          <br />
+          <a href="https://github.com/MohnDoe/poe-passive-tree" target="blank">Source code</a>
+        </p>
+        <PassiveTreeCanvas />
+      </div>
     </main>
   </div>
 </template>
@@ -30,6 +46,43 @@ onMounted(() => {
 <style scoped>
 .app-layout,
 main {
-  height: 100%;
+  height: 100sh;
+  width: 100vw;
+}
+
+.header {
+  position: absolute;
+  top: 16px;
+  left: 16px;
+}
+
+.allocation {
+  display: flex;
+  gap: 16px;
+}
+
+.info {
+  opacity: 0.9;
+  text-align: right;
+  position: absolute;
+  top: 16px;
+  right: 16px;
+  color: white;
+  font-family: "Fontin";
+  max-width: 300px;
+  font-size: 14px;
+}
+
+.info a {
+  color: white;
+  font-weight: bold;
+}
+
+.progress {
+  margin: 8px 0;
+  display: inline-block;
+  font-size: 12px;
+  border: 1px solid #ffffffa0;
+  padding: 8px;
 }
 </style>
